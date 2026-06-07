f460c4eac5cf4a3e6598d9c1d1f4d749c80706ac
Ashutosh902 <ashutoshindian@hotmail.com>
2026-06-07 23:19:34 +0530

    Allow DELETE requests in API CORS

--- server/index.ts
+++ server/index.ts
@@ -29,7 +29,7 @@ const clientOrigin = process.env.CLIENT_ORIGIN || "http://localhost:5173";
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", clientOrigin);
   res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,OPTIONS");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Credentials", "true");
   if (req.method === "OPTIONS") {
     return res.sendStatus(204);
