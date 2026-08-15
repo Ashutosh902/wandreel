24ddec7909d07a1652b31d24228dc1d1d6a48ddb
Ashutosh902 <ashutoshindian@hotmail.com>
2026-08-15 20:32:22 +0530

    Simplify splash screen branding

--- src/ui/splash/OpeningSplash.tsx
+++ src/ui/splash/OpeningSplash.tsx
@@ -1,6 +1,4 @@
-import openingSplashLogo from "../assets/brand/opening-splash-logo.png";
-import wordmark from "../assets/brand/wordmark.svg";
-import tagline from "../assets/brand/tagline.svg";
+import openingSplashMark from "../assets/brand/opening-splash-mark.png";
 
 type OpeningSplashProps = {
   onComplete?: () => void;
@@ -12,13 +10,11 @@ export function OpeningSplash({ onComplete }: OpeningSplashProps) {
       <div className="splash-brand-lockup">
         <div className="splash-image-shell">
           <div className="splash-image-glow" aria-hidden="true" />
-          <div className="splash-mark-frame" aria-hidden="true">
-            <img className="splash-image splash-image-mark" src={openingSplashLogo} alt="" />
-          </div>
+          <img className="splash-image splash-image-mark" src={openingSplashMark} alt="Wandreel" />
         </div>
         <div className="splash-text-lockup">
-          <img className="brand-wordmark splash-wordmark" src={wordmark} alt="Wandreel" />
-          <img className="brand-tagline splash-tagline" src={tagline} alt="From scroll to stroll" />
+          <h1 className="splash-wordmark">WANDREEL</h1>
+          <p className="splash-tagline">FROM SCROLL TO STROLL</p>
         </div>
       </div>
     </section>
